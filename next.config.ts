863ae40f186f811c03cd38ce590d08722e2395e9
Scarlet <spinson78@gmail.com>
2026-06-29 11:14:49 -0500

    hotfix: remove www redirect causing loop

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.thehungryroostertx.com" }],
-        destination: "https://thehungryroostertx.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
